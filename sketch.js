dea54deaa1a8c759354bf7cc2bc43f15dd4884b4
Monique Dumont <johnny.mentz@outlook.fr>
2020-06-03 00:34:18 +0200

    Prototype finished

--- sketch.js
+++ sketch.js
@@ -59,10 +59,16 @@ var scales = [[1, 2, 4, 6, 8, 9, 11],   //A Major
               [0, 2, 5, 7, 10],         //G
               [1, 3, 6, 8, 11],         //G#
             ];
+var scalesNames = ["amaj", "asmaj", "bmaj", "cmaj", "csmaj", "dmaj", "dsmaj", "emaj", "fmaj", "fsmaj", "gmaj", "gsmaj",
+                   "amajpen", "asmajpen", "bmajpen", "cmajpen", "csmajpen", "dmajpen", "dsmajpen", "emajpen", "fmajpen", "fsmajpen", "gmajpen", "gsmajpen",
+                   "amin", "asmin", "bmin", "cmin", "csmin", "dmin", "dsmin", "emin", "fmin", "fsmin", "gmin", "gsmin",
+                   "aminpen", "asminpen", "bminpen", "cminpen", "csminpen", "dminpen", "dsminpen", "eminpen", "fminpen", "fsminpen", "gminpen", "gsminpen"];
 
 var now = 0;
 var t = audioContext.currentTime;
 var mainTrack = [];
+var harmonyTrack = [];
+var hasHarmonyWrittenOnce = false;
 var isTrackPlaying = false;
 var recursiveID;
 
@@ -76,6 +82,10 @@ function setup() {
   canvas.parent('sketch-holder');
   cols = floor(width/w);
   rows = floor(height/w);
+
+  $("#PlayWholeTrack").hide();
+  $("#PlayMainTrack").hide();
+  $("#PlayHarmony").hide();
 
   for (var j = 0; j < rows; j++) {
     for (var i = 0; i < cols; i++) {
@@ -100,11 +110,12 @@ function draw() {
   fill(255);
   stroke(0);
   text("FPS: " + fps.toFixed(2), 10, height - 10);
+  text("Mouse: (" + mouseX.toString() + ", " + mouseY.toString() + ")", width - 100, height - 10);
   
 }
 
 
-//ToDo : sent which tile needs to be clicked
+//TODO: send which tile needs to be clicked
 function mousePressed() {
   for (var i = 0; i < grid.length; i++) {
     grid[i].clicked();
@@ -158,8 +169,8 @@ function Cell(i, j) {
   this.clicked = function() {
     var x = this.i * w;
     var y = this.j * w;
-    var notePitch = getPitch(y);
-    var noteStartTime = getStartTime(x);
+    var notePitch = getPitchByPosition(y);
+    var noteStartTime = getStartTimeByPosition(x);
     var noteDuration = getNoteDuration(tileSize);
     var tileCheck = checkForExistentTiles(i * w, j *w);
 
@@ -171,17 +182,29 @@ function Cell(i, j) {
         mainTrack.push([notePitch, noteStartTime, noteDuration]);
         hideScalesButtons(notePitch % 12);
         if (tileCheck == -1) {
-          var tile = new Tile(i, j, tileSize);
+          var tile = new Tile(x, y, tileSize, 255, 127, 80, 255, 0, 0);
           tiles.push(tile);
         }
       }
       else {
         cancel();
         this.isClicked = false;
-        var noteIndex = findObjectIndex(notePitch, noteStartTime);
+        var noteIndex = findObjectIndex(notePitch, noteStartTime, mainTrack);
         if (noteIndex > -1) {
           mainTrack.splice(noteIndex, 1);
+          if (!mainTrackContainsNote(notePitch)) {
+            if (mainTrack.length == 0) {
+              $(".container").show();
+            }
+            else {
+              showScalesButtons(notePitch % 12);
+            }
+          }
         }
+        noteIndex = findObjectIndex(notePitch, noteStartTime, harmonyTrack);
+        if (noteIndex > -1) {
+          harmonyTrack.splice(noteIndex, 1);
+        } 
         if (tileCheck > -1) {
           tiles.splice(tileCheck, 1);
 
@@ -192,20 +215,27 @@ function Cell(i, j) {
 
 }
 
-function Tile(i, j, tileW) {
-  this.x = i * w;
-  this.y = j * w;
+function Tile(x, y, tileW, r, g, b, outR, outG, outB) {
+  this.x = x;
+  this.y = y;
   this.tileWidth = tileW;
+  this.r = r;
+  this.g = g;
+  this.b = b;
+  this.outR = outR;
+  this.outG = outG;
+  this.outB = outB;
 
   this.show = function() {
-    stroke(255, 0, 0);
-    fill(255, 127, 80);
+    stroke(this.outR, this.outG, this.outB);
+    fill(this.r, this.g, this.b);
     rect(this.x, this.y, this.tileWidth * 24, w);
   }
 }
 
+//check if there is a tile that starts at (x, y) coordinates. Currently only works with the start of the tile
+//TODO: make it work when clicking anywhere on the tile, with any pointer size
 function checkForExistentTiles(x, y) {
-  //console.log("Number of tiles: " + tiles.length.toString());
   for (var i = 0; i < tiles.length; i++) {
     if (tiles[i].x == x && tiles[i].y == y) {
       return i;
@@ -214,6 +244,7 @@ function checkForExistentTiles(x, y) {
   return -1;
 }
 
+//Tile size preview
 function drawTilePreview() {
   var x = floor(mouseX / 24) * 24;
   var y = floor(mouseY / 24) * 24;
@@ -221,6 +252,13 @@ function drawTilePreview() {
   strokeWeight(2);
   noFill();
   rect(x, y, tileSize * 24, w);
+}
+
+function drawHarmonyTrack() {
+  for (var i = 0; i < harmonyTrack.length; i++) {
+    var tile = new Tile(getPositionByStartTime([harmonyTrack[i][1]]), getPositionByPitch(harmonyTrack[i][0]), getTileSizeByDuration(harmonyTrack[i][2]), 50, 205, 50, 0, 100, 0);
+    tiles.push(tile);
+  }
 }
 
 function playNote(pitch, when, dur) {
@@ -232,38 +270,53 @@ function cancel() {
   player.cancelQueue(audioContext);
 }
 
-function playTrack() {
+//TODO: Add a condition so it can be clicked only once
+function playMainTrack() {
   isTrackPlaying = true;
-  mainTrack.sort(function(a, b) {return a[1] - b[1]})
-  playTrackRecursively(0);
+  mainTrack.sort(function(a, b) {return a[1] - b[1]});
+  playTrackRecursively(0, mainTrack);
 }
 
-function playTrackRecursively(id) {
-  var delay = 0;
-  var notesBatch = getNotesOfSameStartTime(mainTrack[id][1]);
+function playHarmonyTrack() {
+  isTrackPlaying = true;
+  harmonyTrack.sort(function(a, b) {return a[1] - b[1]});
+  playTrackRecursively(0, harmonyTrack);
+}
+
+function playWholeTrack() {
+  isTrackPlaying = true;
+  playTrackRecursively(0, mainTrack.concat(harmonyTrack).sort(function(a, b) {return a[1] - b[1]}));
+}
+
+//Recursively go through all the track. At each timestamp, play all the note that start at the same time
+//TODO: Currently start playing at the first note, make it so it starts to play at the beginning.
+function playTrackRecursively(id, track) {
+  var notesBatch = getNotesOfSameStartTime(track[id][1], track);
   playNotesInBatch(notesBatch);
-  if (id + notesBatch.length < mainTrack.length && isTrackPlaying){
-    delay = mainTrack[id + notesBatch.length][1] - mainTrack[id][1];
+  if (id + notesBatch.length < track.length && isTrackPlaying){
+    delay = track[id + notesBatch.length][1] - track[id][1];
     recursiveID = setTimeout(function() {
-      playTrackRecursively(id + notesBatch.length);
+      playTrackRecursively(id + notesBatch.length, track);
     }, delay * 1000);
   }
 }
 
+//Play notes of notes array in a for loop
 function playNotesInBatch(notes) {
   for (var i = 0; i < notes.length; i++) {
     playNote(notes[i][0], notes[i][1], notes[i][2]);
   }
 }
 
-function getNotesOfSameStartTime(startTime) {
+//Get all the notes that start at the same time
+function getNotesOfSameStartTime(startTime, track) {
   var indexArray = []
-  for (var i = 0; i < mainTrack.length; i++) {
-    if (mainTrack[i][1] == startTime) {
+  for (var i = 0; i < track.length; i++) {
+    if (track[i][1] == startTime) {
       indexArray.push(i);
     }
   }
-  var batch = mainTrack.slice(indexArray[0], indexArray[indexArray.length - 1] + 1);
+  var batch = track.slice(indexArray[0], indexArray[indexArray.length - 1] + 1);
   return batch;
 }
 
@@ -279,38 +332,88 @@ function resetSheet() {
       grid[i].isClicked = false;
   }
   mainTrack = [];
+  harmonyTrack = [];
   tiles = [];
+  $(".container").show();
+  $("#Play").show();
+  $("#PlayWholeTrack").hide();
+  $("#PlayMainTrack").hide();
+  $("#PlayHarmony").hide();
 }
 
-function getPitch(y) {
+function harmonize() {
+  var scaleValue = $("input[name='radio']:checked").val();
+  var radioValue = $("input[name='harmony']:checked").val();
+  harmonyTrack = [];
+  if (hasHarmonyWrittenOnce) {
+    tiles.splice(tiles.length / 2, tiles.length / 2);
+  }
+  hasHarmonyWrittenOnce = true;
+  if (radioValue == 0) {
+    console.log("Scale n°:" + scaleValue.toString());
+    console.log(scales[scaleValue]);
+    getThirdHarmonyTrack(scales[scaleValue]);
+  }
+  else if (radioValue == 1) {
+    getFourthHarmonyTrack();
+  }
+  else {
+    getFifthHarmonyTrack();
+  }
+  drawHarmonyTrack();
+  $("#Play").hide();
+  $("#PlayWholeTrack").show();
+  $("#PlayMainTrack").show();
+  $("#PlayHarmony").show();
+}
+
+function getPitchByPosition(y) {
   return 78 - floor(y / 24);
 }
 
-function getStartTime(x) {
+function getPositionByPitch(y) {
+  return (78 - y) * 24;
+}
+
+function getStartTimeByPosition(x) {
   return 0.25 * floor(x / 24);
+}
+
+function getPositionByStartTime(x) {
+  return x * 24 / 0.25;
 }
 
 function getNoteDuration(l) {
   return 0.25 * l;
 }
 
-function findObjectIndex(y, x) {
-  for (var i = 0; i < mainTrack.length; i++) {
-    if (mainTrack[i][0] == y && mainTrack[i][1] == x) {
+function getTileSizeByDuration(d) {
+  return d / 0.25;
+}
+
+function findObjectIndex(y, x, track) {
+  for (var i = 0; i < track.length; i++) {
+    if (track[i][0] == y && track[i][1] == x) {
       return i;
     }
   }
   return -1;
 }
 
+//Hide the scale radio buttons that don't contain the note passed as a parameter
 function hideScalesButtons(pitch) {
   for (let scale of findScalesWithoutNote(pitch)) {
-    //console.log(scale);
-    
-    //$(".container :input").filter(function(){return this.value==scale.toString()}).hide();
-    //console.log($(".container :input[value='" + scale.toString() + "']").val());
-    $("#amajor").hide();
+    $("#" + scalesNames[scale]).hide();
+    //console.log("Hiding " + scalesNames[scale] + " scale");
+  }
+}
 
+
+//Show the scale radio buttons that contain the note passed as a parameter
+function showScalesButtons(pitch) {
+  for (let scale of findScalesWithNote(pitch)) {
+    $("#" + scalesNames[scale]).show();
+    //console.log("Showing " + scalesNames[scale] + " scale");
   }
 }
 
@@ -321,3 +424,64 @@ function* findScalesWithoutNote(pitch) {
     }
   }
 }
+
+
+function* findScalesWithNote() {
+  for (var i = 0; i < scales.length; i++) {
+    if (scaleContainsMainTrack(i)) {
+      yield i;
+    }
+  }
+}
+
+function scaleContainsMainTrack(scaleIndex) {
+  for (var i = 0; i < mainTrack.length; i++) {
+    if (!scales[scaleIndex].includes(mainTrack[i][0] % 12)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//Check if the array mainTrack contains the note passed as a parameter
+function mainTrackContainsNote(note) {
+  for (var i = 0; i < mainTrack.length; i++) {
+    if (mainTrack[i][0] == note) {
+      return true;
+    }
+  }
+  return false;
+}
+
+//Return value to the radio button selected. For scales, name is radio, for harmonies, name is harmony
+/* function getSelectedButtonValue(name) {
+  return $("input[name='" + name + "']:checked").val()
+} */
+
+function getThirdHarmonyTrack(scale) {
+  var noteIndex;
+  var third = 0;
+  for (var i = 0; i < mainTrack.length; i++) {
+    noteIndex = scale.indexOf(mainTrack[i][0] % 12);
+    third = scale[(noteIndex + 2) % scale.length];
+    if (third < mainTrack[i][0]) {
+      while(third < mainTrack[i][0]) {
+        third += 12;
+      }
+    }
+    harmonyTrack.push([third, mainTrack[i][1], mainTrack[i][2]]);
+  }
+}
+
+function getFourthHarmonyTrack() {
+  for (var i = 0; i < mainTrack.length; i++) {
+    harmonyTrack.push([mainTrack[i][0] + 5, mainTrack[i][1], mainTrack[i][2]]);
+  }
+}
+
+function getFifthHarmonyTrack() {
+  for (var i = 0; i < mainTrack.length; i++) {
+    harmonyTrack.push([mainTrack[i][0] + 7, mainTrack[i][1], mainTrack[i][2]]);
+  }
+}
+
